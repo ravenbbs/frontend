2f0ad8cbddc15c781f444d937a1137c5e0b8b56f
ravenbbs <julioc3bgud@gmail.com>
2024-08-15 20:22:43 -0500

    add daisy ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './Card'
+import Form from './Form'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [submittedData, setSubmittedData] = useState<{ name: string; bio: string } | null>(null);
+
+  const handleFormSubmit = (data: { name: string; bio: string } | null) => {
+    if (data) {
+      setSubmittedData(data);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App p-4 max-w-lg mx-auto">
+    <Form onSubmit={handleFormSubmit} />
+    {submittedData && (
+      <div className="mt-6">
+        <Card name={submittedData.name} bio={submittedData.bio} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-    <form></form>
-    <Card/>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    )}
+  </div>
   )
 }
 
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,10 +1,28 @@
 
-const Card = () => {
-  return (
-    <div>
-      Card
-    </div>
-  )
+interface CardProps {
+  name: string;
+  bio: string;
 }
 
-export default Card
+const Card = ({ name, bio }: CardProps) => {
+  return (
+    <div className="card bg-base-100 w-96 shadow-xl">
+  <figure className="px-10 pt-10">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes"
+      className="rounded-xl" />
+  </figure>
+  <div className="card-body items-center text-center">
+    <h2 className="card-title">{name}</h2>
+    <p>{bio}</p>
+    <div className="card-actions">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+
+  );
+};
+
+export default Card;
